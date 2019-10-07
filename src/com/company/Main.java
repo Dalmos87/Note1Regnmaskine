fc2f72f4c9d5943d73e8e7b556c4217a41ca2eec
Dalmos87 <dalmozdk@gmail.com>
2019-10-07 13:14:23 +0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,36 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int a = input.nextInt();
+        input.nextLine();
+        String symbol = input.nextLine();
+        int b = input.nextInt();
+
+
+        input.close();
+
+        if((symbol.equals("+"))){
+            System.out.println(pluser(a,b));
+        }
+        else if((symbol.equals("-"))){
+            System.out.println(pluser(a,b));
+        }
+        else if((symbol.equals("/"))){
+            System.out.println(pluser(a,b));
+        }
+        else if((symbol.equals("*"))){
+            System.out.println(pluser(a,b));
+        }
+    }
+
+    public static int pluser(int x, int y){
+
+       return x+y;
+    }
+
+}
